0f676d3f4762430b68ef4ecf99ae5e0d51f41f14
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-09-25 09:16:51 +0200

    update the front :)

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
-        <button className="btn btn-primary">dsjkfdjfk</button>
+        <button className="btn btn-primary">Click to see nothing :) </button>
 
       </div>
     </div>
