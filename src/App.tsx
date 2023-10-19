e62687cf06f310d70e25382db11c91ca94de15cc
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-10-19 18:03:06 +0200

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="bg-neutral">
       <div className="flex flex-col items-center gap-3">
         <h1 className="text-3xl font-bold underline">
-          Hello world!
+          Pavings (beta)
         </h1>
         <div>
           <label>Width</label>
