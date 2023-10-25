d0cf28e02539d4ce75e8d0ac5c765d73fb90f1c1
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-10-26 00:34:00 +0200

    make padding on x or y

--- src/components/pavings/pavingsRenderer.ts
+++ src/components/pavings/pavingsRenderer.ts
@@ -1,18 +1,30 @@
 
 import { square } from "./index";
 
-function render(context : CanvasRenderingContext2D, width: number, height: number, pavingFunction: any) {
-    const pavingWidth = 32;
-    for(let x = 0; x < width; x+= pavingWidth) {
-        for(let y = 0; y < height; y+=pavingWidth) {
-            pavingFunction(context, x, y, pavingWidth)
-        }
-    }
-}
+
+function render(
+    context : CanvasRenderingContext2D,
+    width: number,
+    height: number,
+    pavingWidth: number,
+    pavingFunction: (context : CanvasRenderingContext2D, x: number, y: number) => void)
+{
+    const pavingHeight = pavingWidth;
+
+    const xOffset = (width % pavingWidth) / 2;
+    const yOffset = (height % pavingHeight) / 2;
+    for(let x = xOffset; x < (width - xOffset); x+= pavingWidth) {
+        for(let y = yOffset; y < (height - yOffset); y+=pavingWidth) {
+            pavingFunction(context, x, y)
+         }
+     }
+ }
+
 
 export function renderSquare(context : CanvasRenderingContext2D, width: number, height: number) {
-    function pavingFunction(context: CanvasRenderingContext2D, x : number, y : number, pavingWidth: number) {
+    const pavingWidth = 32;
+    function pavingFunction(context: CanvasRenderingContext2D, x : number, y : number) {
         square(context, { x, y }, {color: "red", outline: "green"}, pavingWidth);
     }
-    return render(context, width, height, pavingFunction);
+    return render(context, width, height, pavingWidth, pavingFunction);
 }
